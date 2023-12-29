895e77f1ce129aa74338aa8d5540b565e4e8060b
Samuel Seng <djsamseng@gmail.com>
2023-12-29 16:55:28 +0700

    Add new favicon

---
+++ src/lib/components/elipse.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let left: string;
+  export let top: string;
+  export let scale: number;
+
+  const width = 210 / scale;
+  const height = 110 / scale;
+  const viewBox = `0 -50 ${width} ${height}`;
+  const yAdjust = (1 - scale) * 100;
+  const cy = 50 + yAdjust;
+</script>
+
+<svg viewBox={viewBox} class="absolute opacity-50" style={`left: ${left}; top: ${top};`}>
+  <ellipse cx="100" cy={cy} rx="100" ry="50" style={`fill: none; stroke: #fff; stroke-width: 1; transform: rotate3d(1, 1, 1, -45deg);`}/>
+</svg>
---
+++ src/lib/components/galaxy.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { scale } from "svelte/transition";
+import Elipse from "./elipse.svelte";
+</script>
+
+<div class=" bg-[#131717]">
+  <div class="ml-4 p-4 text-xl text-white">Experience</div>
+</div>
+<div class="bg-[#131717] relative h-[600px] z-10">
+
+  <Elipse left="" top="0%" scale={1} />
+  <div class="absolute text-gray-300 z-20 left-[60%] top-[10%] text-wrap whitespace-normal max-w-[250px] flex flex-col">
+    <span class="">Bloomberg LP</span>
+    <span class="">5+ Years</span>
+    <span class="">Software Engineer / Team Lead</span>
+
+  </div>
+  <Elipse left="40%" top="10%" scale={0.2} />
+
+
+</div>
