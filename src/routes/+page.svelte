41c48e2de2e677c4bb811361ec5b06bd0b271c78
Samuel Seng <djsamseng@gmail.com>
2023-12-29 13:15:40 +0700

    Add keyboard with typing characters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+
+<script lang="ts">
+	import TypingKeyboard from "$lib/components/typing-keyboard.svelte";
+
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<TypingKeyboard />
