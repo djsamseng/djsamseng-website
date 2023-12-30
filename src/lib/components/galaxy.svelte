1e33e475b973e7dbe3977d5170349fa21f34a979
Samuel Seng <djsamseng@gmail.com>
2023-12-30 15:59:57 +0700

    Add threejs globe with experience

--- src/lib/components/galaxy.svelte
+++ src/lib/components/galaxy.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { scale } from "svelte/transition";
 import Elipse from "./elipse.svelte";
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 
 <script lang="ts">
-	import Galaxy from "$lib/components/galaxy.svelte";
   import TypingKeyboard from "$lib/components/typing-keyboard.svelte";
   import CardGameSkills from "$lib/components/card-game-skills.svelte";
+	import Globe3d from "$lib/components/globe3d.svelte";
 
 </script>
 
@@ -15,5 +15,8 @@
 </div>
 
 <div class="mt-20">
-  <Galaxy />
+  <div class="bg-[#131717] text-white p-4">
+    <span class="text-xl">Experience</span>
+  </div>
+  <Globe3d />
 </div>
