698815d92e3103e1f565b2fecc6c15fcb3e9051f
Samuel Seng <djsamseng@gmail.com>
2023-12-30 16:37:34 +0700

    Lazy load threejs globe when scrolling into view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,34 @@
 <script lang="ts">
   import TypingKeyboard from "$lib/components/typing-keyboard.svelte";
   import CardGameSkills from "$lib/components/card-game-skills.svelte";
+	import { onMount } from "svelte";
 
-  const LazyGlobe = () => import("$lib/components/globe3d.svelte");
+  let LazyGlobe: any;
+  let experienceDiv: HTMLDivElement;
+  const loadLazyGlobe = () => import("$lib/components/globe3d.svelte");
+
+  async function onExperienceInView(entries: IntersectionObserverEntry[]) {
+    if (LazyGlobe) {
+      return;
+    }
+    if (entries.length === 0 || entries[0].intersectionRatio <= 0) {
+      return;
+    }
+    console.log("Loading globe");
+    LazyGlobe = await loadLazyGlobe();
+  }
+
+
+  onMount(() => {
+    const observer = new IntersectionObserver(onExperienceInView, {
+      root: null,
+      threshold: 0.1,
+    });
+    observer.observe(experienceDiv)
+  })
 </script>
 
-<div class="">
+<div class="min-h-[100vh]">
   <TypingKeyboard />
 </div>
 
@@ -15,7 +38,10 @@
 </div>
 
 <div class="mt-20">
-  <div class="bg-[#131717] text-white p-4">
+  <div class="bg-[#131717] text-white p-4" bind:this={experienceDiv} >
     <span class="text-xl">Experience</span>
+    {#if LazyGlobe}
+      <LazyGlobe.default />
+    {/if}
   </div>
 </div>
